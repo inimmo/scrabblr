f9b396481a0ab344bb597e7be2b6e48e24d32149
Iain Nimmo <iain.nimmo@coniq.com>
2021-08-09 16:18:03 +0100

    Add dictionary

---
+++ word.php
@@ -0,0 +1,63 @@
+<?php
+$word = $_GET['word'];
+
+$dom = new DOMDocument;
+$src = file_get_contents("compress.zlib://http://1word.ws/{$word}");
+
+$dom->loadHTML($src);
+$xpath = new DOMXPath($dom);
+
+$header = $xpath->query("//h1");
+$def = $xpath->query("//h1/following-sibling::ul");
+
+$valid = strpos($header[0]->nodeValue, "is a valid") !== false;
+$defs = $def[0]->childNodes;
+
+$defs = array_map(function ($e) {
+    return $e->nodeValue;
+}, iterator_to_array($defs));
+?>
+
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <title>Is <?= htmlentities($word); ?> valid in Scrabble?</title>
+        <style type="text/css">
+            * {
+                font-family: Georgia, serif;
+            }
+            h1 {
+                font-size: 22pt;
+            }
+            li {
+                font-size: 16pt;
+            }
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+        </style>
+    </head>
+    <body>
+    <?php if ($valid): ?>
+        <h1>Hooray you are good at Scrabble!</h1>
+    <?php else: ?>
+        <h1>Oh no you are bad at Scrabble!</h1>
+    <?php endif; ?>
+
+    <?php if ($defs): ?>
+        <ul>
+            <?php foreach ($defs as $def): ?>
+            <li
+                <?php
+                $pieces = array_map(function ($piece) { return strlen($piece) > 3
+                    ? "<a href='word/$piece'>{$piece}</a>"
+                    : $piece;
+                }, explode(' ', $def));
+
+                echo implode(' ', $pieces);
+                ?>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    </body>
